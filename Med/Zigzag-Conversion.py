26f543dfd5d6463cc2c3dd88e2efd8f181f54dbd
AhmedI968 <86612161+AhmedI968@users.noreply.github.com>
2025-05-11 19:22:31 -0600

    Completed the zigzag problem on leetcode

---
+++ Med/Zigzag-Conversion.py
@@ -0,0 +1,27 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        # Check if len is less than numRows or its length is 1
+        if numRows == 1 or len(s) <= numRows:
+            return s
+        
+        rowString = ['' for _ in range(numRows)]
+
+        # We switch directions when going downwards or upwards
+        direction = 1
+        currentRow = 0
+
+        for character in s:
+            rowString[currentRow] += character
+
+            # Simulate going upwards and downwards in a zigzag manner
+            if currentRow == 0:
+                direction = 1
+            elif currentRow == (numRows-1):
+                direction = -1
+
+            currentRow += direction
+
+        result = ''
+        for row in rowString:
+            result += row
+        return result
